e6863d7630546785c29d5021b12f9b1ad3bf45a1
TokiSeven <tokiseven3@gmail.com>
2017-03-29 02:51:47 +0300

    Prepared project for github

---
+++ dev/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Grid, Row, Col} from 'react-bootstrap';
+
+import Task from './task/';
+
+class Page extends React.Component{
+    render(){
+        return(
+            <Grid>
+                <Row>
+                    <Col xs = {12} sm = {12} className = "text-center">
+                        <h1>Автоматы с магазинной памятью</h1>
+                        <h2>Рябцев Владимир Дмитриевич.</h2>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs = {12} sm = {12} className = "text-center">
+                        <h4>
+                            Разработать алгоритм и программу синтаксически управляемой трансляции простых операторов присваивания, содержащих как целые, так и вещественные переменные. В состав выражения могут входить операции сложения, вычитания, умножения и унарный минус.
+                        </h4>
+                        <h4>
+                            2. Генерация оптимального кода.
+                        </h4>
+                        <Task />
+                    </Col>
+                </Row>
+            </Grid>
+        );
+    }
+}
+
+ReactDOM.render(<Page />, document.getElementById('app'));
